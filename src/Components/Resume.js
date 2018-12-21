cb8f5fa43b0b66cc2357664863c402d4503273f3
konrad0707 <4321konrad@gmail.com>
2018-12-22 00:14:13 +0800

    bug fix

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -4,7 +4,6 @@ class Resume extends Component {
   render() {
 
     if(this.props.data){
-      var skillmessage = this.props.data.skillmessage;
       var education = this.props.data.education.map(function(education){
         return <div key={education.school}><h3>{education.school}</h3>
         <p className="info">{education.degree} <span> | </span><em className="date">{education.graduated}</em></p>
@@ -86,8 +85,6 @@ class Resume extends Component {
 
          <div className="nine columns main-col">
 
-            <p>{skillmessage}
-            </p>
 
 				<div className="bars">
 				   <ul className="skills">
