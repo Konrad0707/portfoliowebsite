cff0285cdd440b4dafd58aab2358cfada4a53765
konrad0707 <4321konrad@gmail.com>
2018-12-21 22:42:36 +0800

    change testimonial section

--- src/Components/Testimonials.js
+++ src/Components/Testimonials.js
@@ -5,7 +5,7 @@ class Testimonials extends Component {
 
     if(this.props.data){
       var interest = this.props.data.interest.map(function(interest){
-        return <div key={interest.name}><h3>{interest.name}</h3>
+        return <div key={interest.name}><h1>{interest.name}</h1>
         </div>
       })
     }
@@ -14,12 +14,9 @@ class Testimonials extends Component {
       <section id="testimonials">
       <div className="text-container">
          <div className="row">
+          <h1><mark>Who Am I?</mark></h1>
 
-            <div className="two columns header-col">
-               <h1><span>Who Am I?</span></h1>
-            </div>
-
-            <div className="ten columns flex-container">
+            <div className="other">
                   <ul className="slides">
                       {interest}
                   </ul>
